6de4421c3b4394cbb6243adbf7907565d199ab2a
unknown <lisa.deslandes@etu.unice.fr>
2021-10-09 16:17:32 +0200

    test d'option de tableau

--- client/src/components/ListeDesRestaurants.vue
+++ client/src/components/ListeDesRestaurants.vue
@@ -32,6 +32,17 @@
           </md-table-cell>
         </md-table-row>
     </md-table>
+
+    <!-- TEST DE CE QUE L'ON PEUT FAIRE-->
+    
+    <div class="phone-viewport">
+      <md-bottom-bar md-sync-route>
+        <img src='../assets/Test.jpg'>
+        <md-bottom-bar-item :to="'/restaurant/' + item._id" exact md-label="Home" md-icon="home"></md-bottom-bar-item>
+        <md-bottom-bar-item to="/components/bottom-bar/posts" md-label="Posts" md-icon="/assets/icon-whatshot.svg"></md-bottom-bar-item>
+        <md-bottom-bar-item to="/components/bottom-bar/favorites" md-label="Favorites" md-icon="favorite"></md-bottom-bar-item>
+      </md-bottom-bar>
+    </div>
   </div>
 </template>
 
@@ -134,4 +145,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.phone-viewport {
+    width: 322px;
+    height: 200px;
+    display: inline-flex; 
+    align-items: flex-end;
+    overflow: hidden;
+    border: 1px solid rgba(#000, .26);
+    background: rgba(#000, .06);
+  }
 </style>
